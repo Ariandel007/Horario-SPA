75dafbf45d30ae1bf7cfae588b427ba13c3bcf48
Ariandel007 <43892122+Ariandel007@users.noreply.github.com>
2020-04-19 04:10:11 -0500

    casi todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { HoraService } from './_services/hora.service';
 import { Hora } from './_models/hora';
 import { Salon } from './_models/salon';
 import { SalonService } from './_services/salon.service';
+import { Curso } from './_models/curso';
+import { CursoService } from './_services/curso.service';
+import { HorarioService } from './_services/horario.service';
 
 @Component({
   selector: 'app-root',
@@ -22,19 +25,24 @@ export class AppComponent implements OnInit{
   salones: Salon[];
   salonPost: any = {};
 
-
-
   //Instructores
-
+    inst1: any = {};
+    inst2: any = {};
 
   //Cursos
+  cursos: Curso[];
+  cursoPost: any = {};
 
+  //mejor horario
+  mejorHorario: any = {};
 
-  constructor(private horaService: HoraService, private salonService: SalonService) {}
+  constructor(private horaService: HoraService, private salonService: SalonService,
+              private cursoService: CursoService, private horarioService: HorarioService) {}
 
   ngOnInit() {
     this.loadHoras();
     this.loadSalones();
+    this.loadCursos();
   }
 
   loadHoras() {
@@ -84,5 +92,43 @@ export class AppComponent implements OnInit{
   }
 
 
+
+  loadCursos() {
+    this.cursoService.getCursos().subscribe((response) =>{
+      this.cursos = response;
+    }, error => {
+      console.log('Error');
+    });
+  }
+
+  crearCurso() {
+    this.cursoPost.instructors = [this.inst1, this.inst2];
+
+    this.cursoService.createCursos(this.cursoPost).subscribe(next => {
+      console.log('todokul con los cursos');
+      this.loadCursos();
+    }, error => {
+      console.log('error');
+    });
+
+  }
+
+  getSelectNav(){
+    return this.selectNav;
+  }
+
+  setSelectNav(n: number){
+    this.selectNav = n;
+    return this.selectNav;
+  }
+
+  loadCalculoMejorHorario() {
+    this.horarioService.getMejorHorario().subscribe((response) => {
+        this.mejorHorario = response;
+        console.log(this.mejorHorario);
+    }, error => {
+      console.log('Error');
+   });
+  }
 
 }
